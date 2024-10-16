c0d356ca7ddce4dc98df224d5c3f69d12c2547ee
Murat Keskin <72799475+murvtkeskin@users.noreply.github.com>
2024-10-16 14:54:37 +0300

    Add files via upload

---
+++ 1.5 Postman/asdas.js
@@ -0,0 +1,15 @@
+public class Test {
+    public static void main(String args[]){
+        pas : 
+        for ( int j = 6; j<8 ,; j++) {
+        
+        for (int i = 0; i<5;i++) {
+            if (i == 3){
+                continue pas;
+            }
+            System.out.print(i);
+        }
+        System.out.print(j);    
+        }
+    }
+}
---
+++ 1.5 Postman/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("<h1>Home Page</h1>");
+});
+
+app.get("/about", (req, res) =>{
+    res.send("BURASI ABOUT PAGE !");
+});
+
+app.post("/register", (req, res) =>{
+    res.sendStatus(201);
+});
+
+app.put("/user/angela", (req, res) => {
+    res.sendStatus(200);
+});
+
+app.listen(port, () =>{
+    console.log("listening on port 3000");
+});
